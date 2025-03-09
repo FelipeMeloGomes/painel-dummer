f62bcfb1350889a573236b9a93f705088e6a8493
Felipe Melo <felipemelogomes36@gmail.com>
2025-03-09 15:46:10 -0300

    feat: add input search user and role

--- src/app/(panel)/profile/page.tsx
+++ src/app/(panel)/profile/page.tsx
@@ -1,18 +1,20 @@
 import colors from "@/constants/colors";
 import { useAuth } from "@/src/context/AuthContext";
 import { supabase } from "@/src/lib/supabase";
-import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Button, Divider, Switch, Text, TextInput } from "react-native-paper";
+import Toast from "react-native-toast-message";
 import DownloadButton from "../../components/Download/page";
 
 export default function Profile() {
   const { setAuth, user } = useAuth();
-  const navigation = useNavigation();
   const [isAdmin, setIsAdmin] = useState(false);
   const [isPremium, setIsPremium] = useState(false);
   const [isFree, setIsFree] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [newRole, setNewRole] = useState<"premium" | "free" | null>(null);
+  const [roleId, setRoleId] = useState<string | null>(null);
 
   type RoleName = "adm" | "premium" | "free";
 
@@ -36,10 +38,12 @@ export default function Profile() {
             .limit(1);
 
           if (error) {
-            Alert.alert(
-              "Erro",
-              "Ocorreu um erro ao verificar o status do usuário.",
-            );
+            Toast.show({
+              type: "error",
+              position: "bottom",
+              text1: "Erro",
+              text2: "Ocorreu um erro ao verificar o status do usuário.",
+            });
             return;
           }
 
@@ -53,10 +57,12 @@ export default function Profile() {
               .single();
 
             if (roleError) {
-              Alert.alert(
-                "Erro",
-                "Ocorreu um erro ao buscar o papel do usuário.",
-              );
+              Toast.show({
+                type: "error",
+                position: "bottom",
+                text1: "Erro",
+                text2: "Ocorreu um erro ao buscar o papel do usuário.",
+              });
               return;
             }
 
@@ -71,24 +77,34 @@ export default function Profile() {
               setIsAdmin(userRole.isAdmin);
               setIsPremium(userRole.isPremium);
               setIsFree(userRole.isFree);
+              setRoleId(roleId);
             } else {
               setIsAdmin(false);
               setIsPremium(false);
               setIsFree(false);
+              setRoleId(null);
             }
           } else {
             setIsAdmin(false);
             setIsPremium(false);
             setIsFree(false);
+            setRoleId(null);
           }
         } catch (error) {
-          Alert.alert(
-            "Erro",
-            "Houve um problema ao buscar as informações do usuário.",
-          );
+          Toast.show({
+            type: "error",
+            position: "bottom",
+            text1: "Erro",
+            text2: "Houve um problema ao buscar as informações do usuário.",
+          });
         }
       } else {
-        Alert.alert("Erro", "Você não está logado.");
+        Toast.show({
+          type: "error",
+          position: "bottom",
+          text1: "Erro",
+          text2: "Você não está logado.",
+        });
         setIsAdmin(false);
         setIsPremium(false);
         setIsFree(false);
@@ -98,12 +114,75 @@ export default function Profile() {
     checkUserInDatabase();
   }, [user]);
 
+  const changeUserRole = async () => {
+    if (!userEmail || !newRole) {
+      Alert.alert(
+        "Erro",
+        "Por favor, forneça o nome do usuário e o novo papel.",
+      );
+      return;
+    }
+
+    try {
+      const { data, error } = await supabase
+        .from("users")
+        .select("id, role_id")
+        .eq("name", userEmail)
+        .limit(1)
+        .single();
+
+      if (error || !data) {
+        Alert.alert("Erro", "Usuário não encontrado.");
+        return;
+      }
+
+      const userId = data.id;
+      setRoleId(data.role_id);
+
+      const { data: roleData, error: roleError } = await supabase
+        .from("roles")
+        .select("id")
+        .eq("name", newRole)
+        .single();
+
+      if (roleError || !roleData) {
+        Alert.alert("Erro", "Erro ao buscar a role.");
+        return;
+      }
+
+      const { error: updateError } = await supabase
+        .from("users")
+        .update({ role_id: roleData.id })
+        .eq("id", userId);
+
+      if (updateError) {
+        Alert.alert("Erro", "Erro ao alterar o papel do usuário.");
+
+        return;
+      }
+
+      Toast.show({
+        position: "bottom",
+        text1: "Sucesso",
+        text2: "O papel do usuário foi alterado com sucesso!",
+      });
+
+      Alert.alert("Sucesso", `O papel do usuário foi alterado com sucesso!`);
+    } catch (error) {
+      Alert.alert("Erro", "Houve um problema ao alterar o papel do usuário.");
+    }
+  };
+
   async function handleSignout() {
     const { error } = await supabase.auth.signOut();
     setAuth(null);
 
     if (error) {
-      Alert.alert("Erro", "Erro ao sair da conta, tente mais tarde.");
+      Toast.show({
+        type: "error",
+        position: "bottom",
+        text1: '"Erro", "Erro ao sair da conta, tente mais tarde."',
+      });
       return;
     }
   }
@@ -124,6 +203,39 @@ export default function Profile() {
       </View>
 
       <View style={styles.buttons}>
+        {isAdmin && (
+          <>
+            <Text variant="displaySmall">Setar Planos</Text>
+            <View style={styles.adminOptions}>
+              <TextInput
+                label="Nome do Usuário"
+                value={userEmail}
+                onChangeText={setUserEmail}
+                placeholder="Nome do Usuário"
+                style={styles.input}
+              />
+              {roleId && (
+                <Text variant="bodyLarge">
+                  Status do Usuário: Role ID - {roleId}
+                </Text>
+              )}
+              <Divider />
+              <View style={styles.switchContainer}>
+                <Text variant="bodyLarge">Free</Text>
+                <Switch
+                  value={newRole === "premium"}
+                  onValueChange={(value) => {
+                    const role = value ? "premium" : "free";
+                    setNewRole(role);
+                    changeUserRole();
+                  }}
+                />
+                <Text variant="bodyLarge">Premium</Text>
+              </View>
+            </View>
+          </>
+        )}
+
         <DownloadButton />
         <Button icon="logout" mode="contained-tonal" onPress={handleSignout}>
           Sair
@@ -140,15 +252,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingTop: 34,
     backgroundColor: colors.zinc,
-  },
-  link: {
-    backgroundColor: colors.green,
-    paddingTop: 14,
-    paddingBottom: 14,
-    textAlign: "center",
-    width: "100%",
-    borderRadius: 8,
-    color: colors.white,
   },
   header: {
     paddingLeft: 14,
@@ -176,5 +279,25 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingLeft: 14,
     paddingRight: 14,
+  },
+  adminOptions: {
+    marginTop: 20,
+    width: "100%",
+  },
+  input: {
+    marginBottom: 10,
+  },
+  roleButton: {
+    marginBottom: 10,
+  },
+  changeRoleButton: {
+    marginTop: 10,
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 10,
+    marginTop: 30,
   },
 });
