316aa3b04af275d55b3a537c73e3780043c8ae7d
Felipe Melo <felipemelogomes36@gmail.com>
2025-03-20 20:21:40 -0300

    Fix: Add flex: 1 to View container around Stack to resolve blank screen issue

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { router, Stack } from "expo-router";
 import { AuthProvider, useAuth } from "../context/AuthContext";
 import { useEffect } from "react";
 import { supabase } from "../lib/supabase";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
@@ -25,21 +26,23 @@ function MainLayout() {
     });
   }, []);
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="screens/(auth)/signup/page"
-        options={{ headerShown: false }}
-      />
+    <View style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="screens/(auth)/signup/page"
+          options={{ headerShown: false }}
+        />
 
-      <Stack.Screen
-        name="screens/(auth)/signin/page"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="screens/(panel)/profile/page"
-        options={{ headerShown: false }}
-      />
-    </Stack>
+        <Stack.Screen
+          name="screens/(auth)/signin/page"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="screens/(panel)/profile/page"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </View>
   );
 }
