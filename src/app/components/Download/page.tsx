f53676502ddc910879680f6a9d6f24439dbbdee8
Felipe Melo <felipemelogomes36@gmail.com>
2025-03-27 13:57:44 -0300

    Refactor: Extract download logic into a custom hook
    - Created the `useDownloadAPK` hook to encapsulate the download logic.
    - Separated concerns in the `DownloadButton` component, making it cleaner and more reusable.
    - Improved code organization and reusability, making it easier to maintain.
    - Added display of remaining download count to enhance the user experience.
    - Improved error handling and visual feedback to prevent unnecessary actions."

--- src/app/components/Download/page.tsx
+++ src/app/components/Download/page.tsx
@@ -1,19 +1,10 @@
-import APK_URLS from "@/constants/apkLinks";
 import colors from "@/constants/colors";
-import { supabase } from "@/src/lib/supabase";
-import * as FileSystem from "expo-file-system";
-import * as Sharing from "expo-sharing";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, Alert, Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
-import Toast from "react-native-toast-message";
-
-const APK_NAME = "painel.apk";
-const MAX_DOWNLOADS = 4;
-type UserRole = "premium" | "free" | null;
+import useDownloadAPK from "../../hooks/useDownloadAPK";
 
 interface DownloadButtonProps {
-  userRole: UserRole;
+  userRole: "premium" | "free" | null;
   userId: string;
 }
 
@@ -21,149 +12,30 @@ export default function DownloadButton({
   userRole,
   userId,
 }: DownloadButtonProps) {
-  const [progressPercentage, setProgressPercentage] = useState(0);
-  const [isDownloading, setIsDownloading] = useState(false);
-  const [downloadCount, setDownloadCount] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (!userId) return;
-
-    const fetchDownloadCount = async () => {
-      const { data, error } = await supabase
-        .from("users")
-        .select("download_count")
-        .eq("id", userId)
-        .single();
-
-      if (error) {
-        console.error("Erro ao buscar downloads:", error);
-        return;
-      }
-
-      setDownloadCount(data?.download_count ?? 0);
-    };
-
-    fetchDownloadCount();
-  }, [userId]);
-
-  async function handleDownload() {
-    if (downloadCount !== null && downloadCount >= MAX_DOWNLOADS) {
-      Alert.alert(
-        "Limite de downloads atingido",
-        "Você já alcançou o limite de downloads.",
-      );
-      return;
-    }
-
-    try {
-      setIsDownloading(true);
-      const fileUri = getDownloadPath();
-
-      const downloadResponse = await startDownload(fileUri);
-      if (!downloadResponse?.uri)
-        throw new Error("Download failed: Empty URI.");
-
-      resetDownloadState();
-      await installAPK(downloadResponse.uri);
-
-      if (downloadCount !== null) {
-        const newDownloadCount = downloadCount + 1;
-        setDownloadCount(newDownloadCount);
-
-        const { data, error } = await supabase
-          .from("users")
-          .update({ download_count: newDownloadCount })
-          .eq("id", userId);
-
-        if (error) {
-          throw new Error("Erro ao atualizar download_count no Supabase.");
-        } else {
-          console.log("Contagem de downloads atualizada no Supabase:", data);
-        }
-      }
-    } catch (error) {
-      handleError(
-        "Download Error",
-        error instanceof Error ? error.message : "An unknown error occurred.",
-      );
-    }
-  }
-
-  function onDownloadProgress({
-    totalBytesWritten,
-    totalBytesExpectedToWrite,
-  }: FileSystem.DownloadProgressData) {
-    const percentage = (totalBytesWritten / totalBytesExpectedToWrite) * 100;
-    setProgressPercentage(percentage);
-  }
-
-  async function installAPK(uri: string) {
-    if (Platform.OS !== "android") {
-      Alert.alert(
-        "Warning",
-        "Automatic installation is only available on Android.",
-      );
-      return;
-    }
-
-    try {
-      const fileInfo = await FileSystem.getInfoAsync(uri);
-      if (!fileInfo.exists) throw new Error("APK file not found.");
-
-      await Sharing.shareAsync(uri, {
-        mimeType: "application/vnd.android.package-archive",
-      });
-    } catch (error) {
-      handleError("Installation Error", "Failed to start APK installation.");
-    }
-  }
-
-  function getDownloadPath() {
-    return FileSystem.documentDirectory + APK_NAME;
-  }
-
-  async function startDownload(fileUri: string) {
-    const downloadResumable = FileSystem.createDownloadResumable(
-      APK_URLS[userRole || "free"],
-      fileUri,
-      {},
-      onDownloadProgress,
-    );
-    return await downloadResumable.downloadAsync();
-  }
-
-  function resetDownloadState() {
-    setProgressPercentage(0);
-    setIsDownloading(false);
-  }
-
-  function handleError(title: string, message: string) {
-    Toast.show({
-      type: "error",
-      position: "bottom",
-      text1: title,
-      text2: message,
-    });
-  }
-
+  const {
+    progressPercentage,
+    isDownloading,
+    handleDownload,
+    downloadCount,
+    MAX_DOWNLOADS,
+  } = useDownloadAPK(userRole, userId);
+  const hasReachedLimit =
+    downloadCount !== null && downloadCount >= MAX_DOWNLOADS;
   return (
     <Button
       mode="contained"
       loading={isDownloading}
       onPress={handleDownload}
-      disabled={isDownloading}
-      buttonColor={colors.green}
+      disabled={isDownloading || hasReachedLimit}
+      buttonColor={hasReachedLimit ? colors.gray : colors.green}
       icon="download"
       style={styles.button}
     >
-      {isDownloading ? (
-        <>
-          {progressPercentage.toFixed(2)}% downloading...
-          <ActivityIndicator color="#000" size="small" />
-        </>
-      ) : (
-        `Download ${userRole === "premium" ? "Premium" : "Free"} APK`
-      )}
+      {hasReachedLimit
+        ? "Limite de downloads atingido"
+        : isDownloading
+          ? `${progressPercentage.toFixed(2)}% baixando...`
+          : `Download ${userRole === "premium" ? "Premium" : "Free"} APK`}
     </Button>
   );
 }
---
+++ src/app/hooks/useDownloadAPK.ts
@@ -0,0 +1,145 @@
+import APK_URLS from "@/constants/apkLinks";
+import { supabase } from "@/src/lib/supabase";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import { useEffect, useState } from "react";
+import { Alert, Platform } from "react-native";
+import Toast from "react-native-toast-message";
+
+const APK_NAME = "painel.apk";
+const MAX_DOWNLOADS = 4;
+
+type UserRole = "premium" | "free" | null;
+
+function useDownloadAPK(userRole: UserRole, userId: string) {
+  const [progressPercentage, setProgressPercentage] = useState(0);
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [downloadCount, setDownloadCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const fetchDownloadCount = async () => {
+      const { data, error } = await supabase
+        .from("users")
+        .select("download_count")
+        .eq("id", userId)
+        .single();
+
+      if (error) {
+        console.error("Erro ao buscar downloads:", error);
+        return;
+      }
+
+      setDownloadCount(data?.download_count ?? 0);
+    };
+
+    fetchDownloadCount();
+  }, [userId]);
+
+  async function handleDownload() {
+    if (downloadCount !== null && downloadCount >= MAX_DOWNLOADS) {
+      Alert.alert(
+        "Limite de downloads atingido",
+        "Você já alcançou o limite de downloads.",
+      );
+      return;
+    }
+
+    try {
+      setIsDownloading(true);
+      const fileUri = getDownloadPath();
+      const downloadResponse = await startDownload(fileUri);
+
+      if (!downloadResponse?.uri)
+        throw new Error("Download failed: Empty URI.");
+
+      resetDownloadState();
+      await installAPK(downloadResponse.uri);
+
+      if (downloadCount !== null) {
+        const newDownloadCount = downloadCount + 1;
+        setDownloadCount(newDownloadCount);
+
+        const { error } = await supabase
+          .from("users")
+          .update({ download_count: newDownloadCount })
+          .eq("id", userId);
+
+        if (error) {
+          throw new Error("Erro ao atualizar download_count no Supabase.");
+        }
+      }
+    } catch (error) {
+      handleError(
+        "Download Error",
+        error instanceof Error ? error.message : "An unknown error occurred.",
+      );
+    }
+  }
+
+  function onDownloadProgress({
+    totalBytesWritten,
+    totalBytesExpectedToWrite,
+  }: FileSystem.DownloadProgressData) {
+    const percentage = (totalBytesWritten / totalBytesExpectedToWrite) * 100;
+    setProgressPercentage(percentage);
+  }
+
+  async function installAPK(uri: string) {
+    if (Platform.OS !== "android") {
+      Alert.alert(
+        "Warning",
+        "Automatic installation is only available on Android.",
+      );
+      return;
+    }
+
+    try {
+      const fileInfo = await FileSystem.getInfoAsync(uri);
+      if (!fileInfo.exists) throw new Error("APK file not found.");
+      await Sharing.shareAsync(uri, {
+        mimeType: "application/vnd.android.package-archive",
+      });
+    } catch {
+      handleError("Installation Error", "Failed to start APK installation.");
+    }
+  }
+
+  function getDownloadPath() {
+    return FileSystem.documentDirectory + APK_NAME;
+  }
+
+  async function startDownload(fileUri: string) {
+    const downloadResumable = FileSystem.createDownloadResumable(
+      APK_URLS[userRole || "free"],
+      fileUri,
+      {},
+      onDownloadProgress,
+    );
+    return await downloadResumable.downloadAsync();
+  }
+
+  function resetDownloadState() {
+    setProgressPercentage(0);
+    setIsDownloading(false);
+  }
+
+  function handleError(title: string, message: string) {
+    Toast.show({
+      type: "error",
+      position: "bottom",
+      text1: title,
+      text2: message,
+    });
+  }
+
+  return {
+    progressPercentage,
+    isDownloading,
+    handleDownload,
+    downloadCount,
+    MAX_DOWNLOADS,
+  };
+}
+export default useDownloadAPK;
